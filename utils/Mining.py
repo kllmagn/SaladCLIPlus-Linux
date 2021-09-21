840d77da503d85d59fff6c7984e0271462b0a475
kllmagn <70798414+kllmagn@users.noreply.github.com>
2021-09-22 01:53:32 +0300

    Update Mining.py

--- utils/Mining.py
+++ utils/Mining.py
@@ -13,7 +13,7 @@ def Salad_Mining():
     color = "\033[32m"  # this is green
 
     os.system('echo ' + color)
-    select = '4'
+    select = '1'
     if select == "1" or select.lower() == "phoenixminer":
         os.system('clear')
         sys.stdout.write("\x1b]2;Mining ethash with PhoenixMiner\x07")
