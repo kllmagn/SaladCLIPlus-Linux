11b9be3b4790d3729f352622d1d6ec446bcf4345
kllmagn <70798414+kllmagn@users.noreply.github.com>
2021-09-22 01:30:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ def print_referral(referral):
 
 def setup():
     sys.stdout.write("\x1b]2;Salad CLI+ SETUP\x07")
-    salad_key = input("Insert your salad Authentication token: ")
-    wallet = input("Insert your salad wallet address: ")
-    refresh_token = input("Insert your salad refresh token: ")
+    salad_key = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsInZlcnNpb24iOiIyIn0=.eyJzZXNzaW9uSGFuZGxlIjoiYWNkNWQ0ODAtZmYyYy00YmJkLWI4YzMtNzMwYjA3ZWE4YmY5IiwidXNlcklkIjoiYmU5OTVhZDMtNmMzMi00NDBkLWEyODUtY2MxNjQ5NjIyOGE3IiwicmVmcmVzaFRva2VuSGFzaDEiOiIyMjY4Y2Q1NzA0NTM1MTY1YmM4NzRiMTk5NmQ0NDRmZTM0NDcxNmI3NTA5ZDkzZTM3YmM2ODVhZjNjMGNhYWFiIiwidXNlckRhdGEiOnt9LCJleHBpcnlUaW1lIjoxNjMyMjY2NjkwODI5LCJ0aW1lQ3JlYXRlZCI6MTYzMjI2MzA5MDgyOSwibG1ydCI6MTYzMjI2MzA5MDgyOX0=.NaweJWaoz3/B+iGPuaNVRTzZLhpg0j8JUb3RZlAnKCI0qe+ESLQKf+rPQfA9jOBl2KwFbfHpED92STS8IhU8x7gwVy3fxV/4wqhaYS8YUS6vcNkJ+d3Era7h/Xnma5snIpr0wGFPhBWaFuQqCcoamjKkFOPtZb2WxAl/VQUOVqJIEbvIs5WKeTYXy+pZc1PDPrfLaI/FRSGL70GCOL8IKa3yYbxISVCvPZG7a1vBs39RW5kd6DZXisVTtK7UNN3Rfp2len8f6lGZ7R/MoMtkk8BUpJrOawi2vzf2fup9qCta1i+ghKb60RpWtPnDnCMf3lXTb29D7bDrZTPIySeGJQ=='
+    wallet = ''
+    refresh_token = 'VcYzKh9+tFAh6fjOZx+GNdsIGcUruJKuX+ekg7ndaccSlx98W14+R0hY5wOho3RXIhFC/H7hV5CRVqqP8JQvvtauxJQ+tl1Vw0hPL8Zf/rfEKtu8yNbwwEDC7mwhzbzxcxWLsWfVFGzzkA3UUl1jPS2ehYr5zVBb0zyUTq8kIyGqYRzqy4FCc681KPEr+lant9E4EU+5+L+KUCYZncYVacw8fXCKeD8JI9hSdvJZ+J7DAe26BV6BafS8UKRhxRuFgMqtvF5x2+EtHRL3Kf9n.ac0d9be84385883b9124525d3be6d9769659f37e176d644f7735b5b540ef84a7.V2'
     data = {
         "salad_key": salad_key,
         "wallet": wallet,
@@ -31,7 +31,7 @@ def setup():
     }
     with open("config.json", "w+") as file:
         json.dump(data, file)
-    python_var = input("\nEnter the name of your python prefix (for example \"python3\"): ")
+    python_var = 'python3'
     print("installing required libraries")
     os.system(f"{python_var} -m pip install --upgrade pip")
     os.system(f"{python_var} -m pip install pyperclip")
@@ -68,7 +68,7 @@ while True:
     dun = False
     while not dun:
         # os.system('clear')
-        action = Start.starting(info)
+        action = 6 #Start.starting(info)
         try:
             int(action)
             dun = True
